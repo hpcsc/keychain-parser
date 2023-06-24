b8013366cb1faf69194c60e29dbb4c4832e02b96
David Nguyen <hpcsc@users.noreply.github.com>
2023-06-24 10:33:19 +1000

    Use urfave/cli to support additional flags/subcommands

--- cmd/keychain-parser/main.go
+++ cmd/keychain-parser/main.go
@@ -4,13 +4,28 @@ import (
 	"github.com/hpcsc/keychain-parser/internal/input"
 	"github.com/hpcsc/keychain-parser/internal/item"
 	"github.com/hpcsc/keychain-parser/internal/output"
+	"github.com/urfave/cli/v2"
+	"log"
 	"os"
 )
 
+var (
+	Version = "main"
+)
+
 func main() {
-	lines := input.From(os.Stdin)
-	items := item.From(lines)
-	if err := output.AsJson(items, os.Stdout); err != nil {
-		panic(err)
+	app := &cli.App{
+		Name:    "keychain-parser",
+		Usage:   "parse output from MacOS security command and output in a more readable format (.e.g. JSON)",
+		Version: Version,
+		Action: func(*cli.Context) error {
+			lines := input.From(os.Stdin)
+			items := item.From(lines)
+			return output.AsJson(items, os.Stdout)
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
